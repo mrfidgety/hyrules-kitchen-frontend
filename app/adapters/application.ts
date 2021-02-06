f5185f0f50c149a7f41508fccb9927d96cf0e420
Tristan Hart <tristanhart@gmail.com>
2021-02-06 13:52:31 +1100

    Add ember power select

--- app/adapters/application.ts
+++ app/adapters/application.ts
@@ -1,6 +1,12 @@
-import JSONAPIAdapter from '@ember-data/adapter/json-api';
+import JSONAPIAdapter from "@ember-data/adapter/json-api";
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
   host = 'http://localhost:3000';
   namespace = 'api/v1';
+}
+
+declare module "@ember-data/adapter/json-api" {
+  interface AdapterRegistry {
+    'application': ApplicationAdapter;
+  }
 }
