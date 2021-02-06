63b5ced5f39b25339b46041ca4ec40a1826f9221
Tristan Hart <tristanhart@gmail.com>
2021-02-06 13:52:55 +1100

    Add image url to ingredients

--- app/models/ingredient.ts
+++ app/models/ingredient.ts
@@ -1,13 +1,20 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
 const { attr } = DS;
 
 export default class Ingredient extends DS.Model {
   @attr('string') name: string;
   @attr('string') category: string;
+  @attr('string') imagePath: string;
   @attr('number') baseHearts: number;
   @attr('number') value: number;
   @attr('number') effectPotency: number;
   @attr('number') time_bonus: number;
+
+  @computed('imagePath')
+  get imageUrl() {
+    return `${this.imagePath}`;
+  }
 }
 
 declare module 'ember-data/types/registries/model' {
